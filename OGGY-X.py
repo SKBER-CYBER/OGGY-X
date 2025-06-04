ee1c318ffc46d9fac66da3e49dbe96213a1301b5
JAKARIYA HASAN <140346514+SKBER-CYBER@users.noreply.github.com>
2025-06-05 04:07:21 +0600

    Update OGGY-X.py

--- OGGY-X.py
+++ OGGY-X.py
@@ -1,1 +1,7 @@
-print('[+] error fix first user')
+import platform,os
+os.system("git pull")
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import xxx1
+elif bit == '32bit':
+    print("SORRY BRO")
